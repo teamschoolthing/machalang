14cb52e1e707b18df0fc751d51c95f47dc60947c
Mihir Maroju <91926675+mikidoodle@users.noreply.github.com>
2023-10-11 13:34:32 +0530

    Factorised Syntax to JSON file

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 var fs = require("fs");
 var c = require("ansi-colors");
-var sampleText = `
-hi macha
-eduthuko macha a=2
-eduthuko macha b=3
-ippo macha (a+b==5)
-{
-    padi macha a+b + " macha"
-} illana macha {
-    padi macha "illana macha"
-}
-bye macha
-`;
+
+var syntax = fs.readFileSync("./syntax.json");
 
 process.argv.forEach(function (val, index, array) {
   if (index == 2) {
     var path = val;
     fs.readFile(path, "utf8", function (err, data) {
       if (err) throw err;
-      console.log(c.yellow(path + " pannitu iruken macha..."));
+      console.log(c.yellow(path + " " + syntax.processDescriptors.interpretationNotice));
       text = data;
       interpret(text);
     });
   }
 });
-function findDataBetweenQuotes(text) {
-  return text.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
-}
 
 function interpret(text) {
   var inputSplit = text.split("\n");
   var output = "";
   var begin = false;
 
-  if (!text.includes("hi macha") || !text.includes("bye macha")) {
-    console.log("Dei macha, hi macha and bye macha use pannu");
+  if (
+    !text.includes(syntax.interpretationTriggers.start) ||
+    !text.includes(syntax.interpretationTriggers.end)
+  ) {
+    console.log(
+      `Dei ${syntax.languageDescriptors.languagePointers}, ${syntax.interpretationTriggers.start} and ${syntax.interpretationTriggers.end} use pannu`
+    );
     return;
   }
   for (var i = 0; i < inputSplit.length; i++) {
+    var trueRegex = new RegExp(syntax.boolean.true, "gm");
+    var falseRegex = new RegExp(syntax.boolean.false, "gm");
+    var nullRegex = new RegExp(syntax.nullValue, "gm");
     inputSplit[i] = inputSplit[i]
       .trim()
-      .replace(/ama/gm, "true")
-      .replace(/poi/gm, "false")
-      .replace(/onnum illa/gm, "null");
+      .replace(trueRegex, "true")
+      .replace(falseRegex, "false")
+      .replace(nullRegex, "null");
 
     if (begin == true) {
-      if (inputSplit[i].includes("eduthuko macha")) {
+      if (inputSplit[i].includes(syntax.variableDeclaration)) {
         //begin interpreting
         var variable = inputSplit[i].split(" ")[2].split("=")[0];
         var value = inputSplit[i].substring(
@@ -54,38 +49,40 @@ function interpret(text) {
         );
 
         output += "\nvar " + variable + "=" + value + ";";
-      } else if (inputSplit[i].includes("padi macha")) {
+      } else if (inputSplit[i].includes(syntax.printStatement)) {
         //print
-        var print = inputSplit[i].replace("padi macha", "");
+        var print = inputSplit[i].replace(syntax.printStatement, "");
         output += "\nconsole.log(" + print + ");";
-      } else if (inputSplit[i].includes("hi macha")) {
+      } else if (inputSplit[i].includes(syntax.interpretationTriggers.start)) {
         begin = true;
-      } else if (inputSplit[i].includes("ippo macha")) {
+      } else if (inputSplit[i].includes(syntax.conditionals.if)) {
         //if condition
-        var condition = inputSplit[i].replace("ippo macha", "");
+        var condition = inputSplit[i].replace(syntax.conditionals.if, "");
         output += "\nif" + condition + "";
-      } else if (inputSplit[i].includes("illana macha")) {
+      } else if (inputSplit[i].includes(syntax.conditionals.else)) {
         //else condition
-        var condition = inputSplit[i].replace("illana macha", "else");
+        var condition = inputSplit[i].replace(syntax.conditionals.else, "else");
         output += "\n" + condition;
-      } else if (inputSplit[i].includes("bye macha")) {
+      } else if (inputSplit[i].includes(syntax.interpretationTriggers.end)) {
         begin = false;
         try {
           eval(output);
         } catch (e) {
           if (e instanceof SyntaxError) {
-            console.log(c.red("ayyo macha :( error:"));
+            console.log(c.red(syntax.processDescriptors.errors.nonDescript));
             console.log(c.bgRedBright(e));
-            console.log(c.yellowBright("spelling check pannu macha"))
+            console.log(
+              c.yellowBright(syntax.processDescriptors.errors.spellCheck)
+            );
           } else {
-            console.log(c.green("wow macha!"));
+            console.log(c.green(syntax.processDescriptors.success));
           }
         }
         break;
       } else {
         output += "\n" + inputSplit[i];
       }
-    } else if (inputSplit[i].includes("hi macha")) {
+    } else if (inputSplit[i].includes(syntax.interpretationTriggers.start)) {
       begin = true;
     }
   }
